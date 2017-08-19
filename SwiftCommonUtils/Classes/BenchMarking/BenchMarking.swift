3d0487584bd2054c86024a0a34aca847832e3598
Michael Henry Pantaleon <me@iamkel.net>
2017-08-19 11:38:21 +0900

    added benchmarking class

---
+++ SwiftCommonUtils/Classes/BenchMarking/BenchMarking.swift
@@ -0,0 +1,23 @@
+//
+//  BenchMarking.swift
+//  Pods
+//
+//  Created by Michael Henry Pantaleon on 2017/08/19.
+//
+//
+
+import QuartzCore
+
+class BenchMarking {
+    
+    /**
+        from https://gist.github.com/kristopherjohnson/4201fbe86473f6edb207
+    */
+    public static func executionTimeInterval(_ block: () -> ()) -> CFTimeInterval {
+        let start = CACurrentMediaTime()
+        block();
+        let end = CACurrentMediaTime()
+        return end - start
+    }
+
+}
